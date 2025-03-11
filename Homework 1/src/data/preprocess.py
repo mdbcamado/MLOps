3fc3abc95d570103ada0151012f6e17f420d16cc
mdbcamado <mdbcamado@gmail.com>
2025-03-11 13:09:41 +0800

    Homework 2
    Homework 2 - Dagster with bonus fastapi

--- Homework 1/src/data/preprocess.py
+++ Homework 1/src/data/preprocess.py
@@ -2,11 +2,18 @@ from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import numpy as np
 import joblib
+from pathlib import Path
 
-def preprocess_data(df: pd.DataFrame, config: dict) -> pd.DataFrame:
+# Get the absolute path to the Project directory
+BASE_DIR = Path(__file__).resolve().parent.parent  # Moves up to src/
+
+# Adjust the file path relative to diabetesProj
+filepath = BASE_DIR.parent / "models/scaler.joblib"
+
+def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
     """Preprocess raw data with scaling and cleaning."""
     scaler = StandardScaler()
     numerical_features = ['mass', 'width', 'height', 'color_score']
     df[numerical_features] = scaler.fit_transform(df[numerical_features])
-    joblib.dump(scaler, config['paths']['scaler_save_path'])
+    joblib.dump(scaler, filepath)
     return df
